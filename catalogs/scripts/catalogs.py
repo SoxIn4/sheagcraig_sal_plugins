3888cb1114dbec4897251c4b2deb6d806f147f82
Packaging Hub <dohare@sgkinc.com>
2025-03-09 03:17:20 -0500

    get catalogs from file system
    simplify and match method for manifests

--- catalogs/scripts/catalogs.py
+++ catalogs/scripts/catalogs.py
@@ -1,29 +1,16 @@
 #!/usr/local/sal/Python.framework/Versions/Current/bin/python3
-
-import os
-import plistlib
+import pathlib
 
 import sal
 
-from Foundation import CFPreferencesCopyAppValue
-
 def main():
-    bundle_id = "ManagedInstalls"
-    manifest =  CFPreferencesCopyAppValue('ClientIdentifier', bundle_id)
-    client_manifest_path = f'/Library/Managed Installs/manifests/{manifest}'
-    
-    if os.path.exists(client_manifest_path):
-        with open(client_manifest_path, 'rb') as cm:
-            client_manifest = plistlib.load(cm)
-    else:
-        client_manifest = {}
-
-    # Drop any blank entries and trim WS.
-    catalogs = [c.strip() for c in client_manifest.get("catalogs", []) if c]
+    catalogs_dir = pathlib.Path('/Library/Managed Installs/catalogs')
+    catalogs = [item.stem for item in catalogs_dir.rglob('*')
+                 if item.is_file()]
     if not catalogs:
-        catalogs = ["NO CATALOGS"]
-    
+        catalogs = ["NO INCLUDED CATALOGS"]
     sal.add_plugin_results('Catalogs', {"Catalogs": "+".join(catalogs)})
+
 
 if __name__ == "__main__":
     main()
